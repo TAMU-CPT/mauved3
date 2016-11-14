130e499dda3e39297688dc152f3c7f86805b1db2
Eleni Mijalis <elenimijalis@gmail.com>
2016-11-14 15:08:52 -0600

    draws genomes relative to longest

--- index.js
+++ index.js
@@ -25,48 +25,86 @@ var rect = svg.append("rect")
 
 container = svg.append("g");
 
-var bars = container.selectAll("rect")
-            .data(data)
-            .enter()
-                .append("rect")
-                .attr("width", 10)
-                .attr("height", 10)
-                .attr("x", function(d, i) {return i*12;})
-                .style("fill", function(nuc) {
-                    switch(nuc) {
-                        case "A":
-                            return "blue";
-                        case "T":
-                            return "red";
-                        case "G":
-                            return "yellow";
-                        case "C":
-                            return "green";
-                    }
-                });
-
-
 function zoomed() {
     tx = d3.event.transform;
     txf = "translate(" + tx.x + " " + tx.y + ") scale(" + tx.k + ")";
     container.attr("transform", txf);
 }
 
-// get fasta data
-//var parseQueryString = function(url) {
-  //var urlParams = {};
-  //url.replace(
-    //new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-    //function($0, $1, $2, $3) {
-      //urlParams[$1] = $3;
-    //}
-  //);
-  //return urlParams;
-//}
+var draw_genomes = function(data) {
+    var genomes = container.selectAll("line")
+                    .data(data)
+                    .enter()
+                        .append("line")
+                        .attr("x1", 30)
+                        .attr("y1", function(d,i) {return 30 + i*100})
+                        .attr("x2", function(d) {return d;})
+                        .attr("y2", function(d,i) {return 30 + i*100})
+                        .attr("stroke-width", 10)
+                        .attr("stroke", "green");
+};
 
-//$.getJSON(parseQueryString(location.search).url, function(json) {
-    //$.get(json.fasta[0], function(fasta_data) {
-        //draw(fasta_data.trim().split(""));
-        //console.log(fasta_data.trim().split(""));
-    //});
-//});
+var draw_features = function(gff3, ratio) {
+
+};
+
+//var bars = container.selectAll("rect")
+            //.data(data)
+            //.enter()
+                //.append("rect")
+                //.attr("width", 10)
+                //.attr("height", 10)
+                //.attr("x", function(d, i) {return i*12;})
+                //.style("fill", function(nuc) {
+                    //switch(nuc) {
+                        //case "A":
+                            //return "blue";
+                        //case "T":
+                            //return "red";
+                        //case "G":
+                            //return "yellow";
+                        //case "C":
+                            //return "green";
+                    //}
+                //});
+
+//get fasta and gff3 data
+var parseQueryString = function(url) {
+  var urlParams = {};
+  url.replace(
+    new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+    function($0, $1, $2, $3) {
+      urlParams[$1] = $3;
+    }
+  );
+  return urlParams;
+}
+
+// find genome with longest length
+var find_longest = function(fasta) {
+    var longest = 0;
+    $.each(fasta, function(key, data) {
+        if (data.length > longest) longest = data.length;
+    });
+    return longest
+}
+
+// adjust pixels to genome length
+var adjust_genomes = function(data, longest) {
+    adjusted_genomes = []
+    $.each(data, function(key, fasta) {
+        adjusted_genomes.push(fasta.length/longest * 900);
+    });
+    return adjusted_genomes;
+};
+
+$.getJSON(parseQueryString(location.search).url, function(json) {
+
+    adjusted_genomes = adjust_genomes(json.fasta, find_longest(json.fasta));
+    draw_genomes(adjusted_genomes);
+
+    $.get(json.gff3[0], function(gff3_data) {
+        //console.log(longest);
+        //console.log(genome_ratios);
+    });
+});
