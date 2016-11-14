d4976a128a2ccada08f3fbe8851173cb850e2224
Eleni Mijalis <elenimijalis@gmail.com>
2016-11-14 11:09:01 -0600

    trying d3 svg

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var zoom = d3.zoom()
+    .scaleExtent([1, 32])
+    .on("zoom", zoomed);
+
+//
+
+var canvas = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .call(zoom);
+
+var draw = function(data) {
+    var bars = canvas.selectAll("rect")
+                .data(data)
+                .enter()
+                    .append("rect")
+                    .attr("width", 10)
+                    .attr("height", 10)
+                    .attr("x", function(d, i) {return i*12;})
+                    .style("fill", function(nuc) {
+                        switch(nuc) {
+                            case "A":
+                                return "blue";
+                            case "T":
+                                return "red";
+                            case "G":
+                                return "yellow";
+                            case "C":
+                                return "green";
+                        }
+                    });
+};
+
+function zoomed() {
+    tx = d3.event.transform;
+    txf = "translate(" + tx.x + " " + tx.y + ") scale(" + tx.k + ")";
+    canvas.attr("transform", txf);
+}
+
+// get fasta data
 var parseQueryString = function(url) {
   var urlParams = {};
   url.replace(
@@ -10,7 +57,7 @@ var parseQueryString = function(url) {
 }
 
 $.getJSON(parseQueryString(location.search).url, function(json) {
-    $.get(json.fasta[0], function(fasta) {
-        console.log(fasta);
+    $.get(json.fasta[0], function(fasta_data) {
+        draw(fasta_data.trim().split(""));
     });
 });
